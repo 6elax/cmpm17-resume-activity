fc5d31078d1ae8419deaa615186fc8242f6923fe
Alexis Luo <--replace-all>
2026-04-06 18:56:03 -0700

    fix: change static to dynamic via ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,51 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+// export interface User {
+//   name: string;
+//   year: number;
+//   major: string;
+// }
+// export const users: User[] = [
+//   {
+//     name: 'Alexis',
+//     year: 1,
+//     major: 'CS'
+//   }
+// ]
+
+// export interface Skill {
+//   title: string;
+//   yearsExp: number;
+//   desc: string; 
+// }
+// export const skills: Skill[] = [
+//   {
+//     title: 'asdf',
+//     yearsExp: 4,
+//     desc: 'paragraph'
+//   },
+//   {
+//     title: 'student',
+//     yearsExp: 10,
+//     desc: 'learning'
+//   }
+// ]
+
 export class AppComponent {
   title = 'cmpm17-resume-activity';
+
+  name: string = "Alexis";
+  
+  year: number = 1;
+  major: string = 'Computer Science';
+
+
+skills: string[] = [
+    'asdf',
+    'paragraph'
+]
+  
+  
 }
