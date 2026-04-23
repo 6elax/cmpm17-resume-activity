e3f554006f945cc8a129396d5211201f1ddfebd7
Alexis Luo <--replace-all>
2026-04-22 18:59:29 -0700

    feat: add signals of work experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, computed, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { Experience } from './experience/experience.model';
+import { EXPERIENCES } from './experience/experience.mock';
 
 @Component({
   selector: 'app-component',
-  imports: [MatExpansionModule],
+  imports: [ MatExpansionModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -55,6 +57,22 @@ export class AppComponent {
   ]
 
 
+  /** Working experiences. */
+  experiences: Experience[] = EXPERIENCES;
+
+  /** Current user name. */
+  currentName: WritableSignal<string> = signal('Jialai');
+
+  // --------- COMPUTED DATA
+
+  /** Select cmpm17 student info */
+  cmpm17student: Signal<Experience|undefined> = computed( () => { 
+    //need |undefined in case "cmpm17student" doesn't exist
+    return this.experiences.find(
+      //exp => exp.name == 'cmpm17-student'
+      exp => exp.name == this.currentName()
+    )
+  } )
 
   
   
